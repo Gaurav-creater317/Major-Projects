7537a02371121b960a9d5cb38f8513aa411538a2
Gaurav_creater317 <gaurav.hld11111@gmail.com>
2024-07-22 23:52:08 +0530

    Project--> Emoji Rating !

---
+++ Emoji Rating/index.js
@@ -0,0 +1,31 @@
+const starsEl = document.querySelectorAll(".fa-star");
+
+const emojisEl = document.querySelectorAll(".fa-regular");
+
+const colorsArray = ["red", "grey" , "blue" , "brown" , "green"];
+
+updateRating(0);
+
+starsEl.forEach((starEl , index) =>{
+   starEl.addEventListener("click" , ()=>{
+       updateRating(index);
+   });
+});
+
+
+function updateRating(index){
+   starsEl.forEach((starEl,idx)=>{
+           if(idx < index + 1 ){
+            starEl.classList.add("active");
+           } else {
+            starEl.classList.remove("active");
+           }
+   });
+
+   emojisEl.forEach((emojiEl)=>{
+          emojiEl.style.transform = `translateX(-${ index * 50 }px)`;
+          emojiEl.style.color = colorsArray[index];
+   })
+
+
+};
